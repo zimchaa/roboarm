08f8147ac284d467cdee4e38277adfeed448241b
Jaison Miller <jaison.miller@gmail.com>
2016-01-08 11:03:53 +0000

    Added some comments around the compliled command hex and removed the unused code

--- roboarm-adv.py
+++ roboarm-adv.py
@@ -24,40 +24,6 @@ robo_arm = usb.core.find(idVendor=0x1267, idProduct=0x000)
 usb_command_array = ['00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00']
 compiled_command = [0, 0, 0]
 
-# movearm object for controlling the Arm
-def move_arm (move_command, app_mode):  # After this, all code until the demo commands must be indented
-
-	# calculate the correct full_command
-	# update the usb_command_array with appropriate value for the command and the right place for it in the array
-	usb_command_array[move_command[1]] = move_command[0];
-
-	#build up the compiled array in parts so that it's readable
-	compiled_command_arm = usb_command_array[0] + usb_command_array[1] + usb_command_array[2] + usb_command_array[3]
-	compiled_command_base = usb_command_array[4] + usb_command_array[5] + usb_command_array[6] + usb_command_array[7]
-	compiled_command_light = usb_command_array[8] + usb_command_array[9] + usb_command_array[10] + usb_command_array[11]
-
-	compiled_command = [int(compiled_command_arm, 2), int(compiled_command_base, 2), int(compiled_command_light, 2)]
-
-	# Function to start the movement
-	if app_mode == "testing":
-
-		# don't do the USB transfer
-		print compiled_command
-
-	elif app_mode == "live":
-
-		# do the transfer
-		robo_arm.ctrl_transfer(0x40, 6, 0x100, 0, compiled_command, 1000)
-
-    # Sim -> Adv. remove the time constraint - leaving the comment in case we need to implement a safety valve
-	# that turns off any movement past say 5 seconds - that would be as the result of the server and client
-	# loosing connection.
-
-	# Stop the movement after waiting a specified duration
-	# time.sleep(Duration)
-	# ArmCmd = [0, 0, 0]
-	# RoboArm.ctrl_transfer(0x40, 6, 0x100, 0, ArmCmd, 1000)
-
 # Sim -> Adv - the movemap is not needed at the moment, as we're going to recreate a more flexible interface
 # leaving here for reference
 
@@ -145,6 +111,7 @@ def MoveArmInterface():
 		compiled_command_base = usb_command_array[4] + usb_command_array[5] + usb_command_array[6] + usb_command_array[7]
 		compiled_command_light = usb_command_array[8] + usb_command_array[9] + usb_command_array[10] + usb_command_array[11]
 
+		# compose the command and represent the values as hex to send to the arm
 		compiled_command = [hex(int(compiled_command_arm, 2)), hex(int(compiled_command_base, 2)), hex(int(compiled_command_light, 2))]
 
 		# Function to start the movement
